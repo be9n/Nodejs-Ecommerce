e306c1bcc319d66f9853cf6eeb431e42ab6ac5f6
be9n <abdulrahman.nas48@gmail.com>
2024-02-08 17:27:06 +0300

    Ready for hosting

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const app = express();
 // rest of the packages
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const rateLimiter = require("express-rate-limit");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const cors = require("cors");
+const mongoSanitize = require("express-mongo-sanitize");
 
 // database
 const connectDB = require("./db/connect");
@@ -21,14 +26,24 @@ const userRoutes = require("./routes/user/userRoutes");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
-app.use(morgan("dev"));
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
+);
+
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
 app.use(cookieParser(process.env.JWT_SECRET_KEY));
 app.use(express.json());
-app.use(express.static('public'))
-
-app.get("/api", (req, res) => {
-  res.send("e-commerce api");
-});
 
 app.use("/api/auth", authRouter);
 // admin router
