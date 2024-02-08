bc7d62adda88253498ebfb6ee9d64e6dad1952c0
be9n <abdulrahman.nas48@gmail.com>
2024-02-08 18:18:23 +0300

    testing

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 5000;
 const start = async () => {
   try {
-    // await connectDB(process.env.MONGO_URL);
+    await connectDB(process.env.MONGO_URL);
     app.listen(port, () => console.log(`Server is running on port ${port}...`));
   } catch (error) {
     console.log(error);
